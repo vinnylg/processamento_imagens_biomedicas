d368eafdf4d86458288ca7fe8f895be5b8b8e42d
Lucas Schoenfelder <les17@inf.ufpr.br>
2020-09-23 13:31:10 -0300

    adaptando pro fractal

--- classification.py
+++ classification.py
@@ -9,88 +9,19 @@ from sklearn import metrics
 from sklearn import svm
 import statistics
 from sklearn.metrics import confusion_matrix
-from extraction import getLBP, getTopHat
+from extraction import getLBP, getTopHat, getFractalDim
 
-# dataset = [
-#     {
-#         'patient_id': 'number for patient identification on dataset',
-#         'data': 'list of all images features, data[i] can be list of histogram, means, etc',
-#         'target': 'target/label for classification of data[i] feature'
-#     }
-#     ... array of patients
-#     ]
-# def runSVM(dataset):
-# 	start_time = time.time()
-# 	preds = []
-# 	values = []
-# 	#prepare model
-# 	model = svm.SVC(kernel='rbf', random_state=0, gamma=1,C=1, decision_function_shape='ovo')
-#
-# 	#for each patient
-# 	for index in range(len(dataset)):
-# 		#split dataset into validation set and training set
-# 		#leave one out
-# 		train = dataset[:index] + dataset[index+1:]
-# 		test = [dataset[index]]
-# 		#k-fold simples
-# 		# train = dataset[20:]
-# 		# test = dataset[:20]
-#
-# 		#prepare data for training
-# 		trainX = []
-# 		trainY = []
-# 		for patient in train:
-# 			for data, target in zip(patient['data'], patient['target']):
-# 				trainX.append(data)
-# 				trainY.append(target)
-#
-# 		trainX = np.array(trainX,dtype='object')
-# 		trainX = normalize(trainX)
-#
-# 		trainY = np.array(trainY,dtype='float')
-#
-# 		print(f"{trainX.shape} features to fit from {len(train)} patients, without patient {index}")
-#
-# 		#fit model
-# 		model.fit(trainX, trainY)
-#
-# 		#prepare validation data
-# 		testX = []
-# 		testY = []
-# 		for patient in test:
-# 			for data, target in zip(patient['data'], patient['target']):
-# 				testX.append(data)
-# 				testY.append(target)
-#
-# 		testX = np.array(testX, dtype='object')
-# 		testX = normalize(testX)
-#
-# 		testY = np.array(testY, dtype='float')
-#
-# 		print(f"{testX.shape} features to test for patient {index}")
-#
-# 		#get predictions for each instance in validation data
-# 		pred = model.predict(testX)
-#
-# 		preds.append(pred)
-# 		values.append(testY)
-# 		print(f"accuracy: {metrics.accuracy_score(testY, pred)}")
-# 		print(f" Time elapsed: {datetime.timedelta(seconds=round(time.time()-start_time))} seconds")
-#
-# 	return preds,values
 def print_confusion_matrix (cm):
     for row in cm:
         print('\t',row)
 
-def main():
+def classify(dataset):
     if(not os.path.isdir('./predictions')):
         os.mkdir('./predictions')
 
     scores = []
 	#prepare model
     model = svm.SVC(kernel='rbf', random_state=0, gamma='scale',C=1, decision_function_shape='ovo')
-
-    dataset = getTopHat()
 
     cm = [[0 for i in range(6)] for j in range(6)]
     #print_confusion_matrix(cm)
@@ -117,8 +48,12 @@ def main():
                 trainY.append(target)
 
         trainX = np.array(trainX,dtype='object')
+        #print(trainX)
+        #print(len(trainX))
+        if trainX.ndim == 1:
+            trainX = trainX.reshape(-1,1)
         trainX = normalize(trainX)
-
+        #print(trainX)
         trainY = np.array(trainY,dtype='float')
 
         print(f"{trainX.shape} features to fit from {len(train)} patients, without patient {index}")
@@ -135,6 +70,8 @@ def main():
                 testY.append(target)
 
         testX = np.array(testX, dtype='object')
+        if testX.ndim == 1:
+            testX = testX.reshape(-1,1)
         testX = normalize(testX)
 
         testY = np.array(testY, dtype='float')
@@ -151,24 +88,32 @@ def main():
         #print('cm')
         #print_confusion_matrix(cm)
 
-        #preds.append(pred)
-        #values.append(testY)
+        preds.append(pred)
+        values.append(testY)
         scores.append(metrics.accuracy_score(testY, pred))
         print(f"accuracy: {metrics.accuracy_score(testY, pred)}")
         print(f" Time elapsed: {datetime.timedelta(seconds=round(time.time()-start_time))} seconds")
 
-    print(np.mean(scores))
-    print_confusion_matrix(cm)
+    #print(np.mean(scores))
+    #print_confusion_matrix(cm)
 
-    #with open('./predictions/svm_tophat.npz', 'wb') as out:
-    #    np.savez(out, preds=preds,values=values)
+    return preds, values, cm, np.mean(scores)
+
+def main():
+    # preds, values, cm, acc = classify(getTopHat())
+    # with open('./predictions/svm_tophat.npz', 'wb') as out:
+    #    np.savez(out, preds=preds,values=values, cm=cm, acc=acc)
     #    print(f"./predictions/svm_tophat.npz saved")
+
+    preds, values, cm, acc = classify(getFractalDim())
+    with open('./predictions/svm_fractaldim.npz', 'wb') as out:
+        np.savez(out, preds=preds,values=values, cm=cm, acc=acc)
+        print(f"./predictions/svm_fractaldim.npz saved")
 
 	# preds, values = runSVM(getLBP())
 	# with open('./predictions/svm_LBP.npz', 'wb') as out:
-	# 	np.savez(out, preds=preds, values=values)
+	# 	np.savez(out, preds=preds,values=values, cm=cm, acc=acc)
 	# 	print(f"./predictions/svm_LBP.npz saved")
-
 
 if __name__ == "__main__":
 	main()
